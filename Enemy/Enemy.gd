12aa28dce9f4c182549ccb57c7971d6d9df7f7c2
Ian <iandbrower@gmail.com>
2022-04-09 12:03:40 -0400

    Complete Basic Solution

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -10,4 +10,12 @@ func _on_Enemy_body_entered(body):
 		var sound = get_node_or_null("/root/Game/Zombie")
 		if sound != null:
 			sound.playing = true
-		queue_free()
+		var _scene = get_tree().change_scene("res://UI/Lose.tscn")
+
+
+
+func _on_Area_body_entered(body):
+	if body.name == "Player":
+		var sound = get_node_or_null("/root/Game/Zombie")
+		if sound != null:
+			sound.playing = true
